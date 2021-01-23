b4e1660654b47a872f03a11d5d05b993f23d333a
shivi0708 <shivi.sv8@gmail.com>
2021-01-23 20:17:08 +0530

    binary tree

---
+++ BT/binarytree.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class BTNode{
+    public:
+        T data;
+        BTNode* left;
+        BTNode *right;
+
+        BTNode(T data){
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+
+        ~BTNode(){
+            delete left;
+            delete right;
+        }
+};
+
+void print(BTNode<int>* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    if(root->left){
+        cout<<"L"<<root->left->data<<" ";
+    }
+    // cout<<endl;
+    if(root->right){
+        cout<<"R"<<root->right->data<<" ";
+    }
+    cout<<endl;
+    print(root->left);
+    print(root->right);
+}
+
+int main(){
+    BTNode<int>*root = new BTNode<int>(1);
+    BTNode<int>*n1 = new BTNode<int>(2);
+    BTNode<int>*n2 = new BTNode<int>(3);
+    root->left=n1;
+    root->right=n2;
+    print(root);
+    delete root;
+}
