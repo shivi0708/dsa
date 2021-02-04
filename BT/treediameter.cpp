26ea7866b108e0645669df8acaa4d1fd170dfdcb
shivi0708 <shivi.sv8@gmail.com>
2021-02-04 23:49:45 +0530

    diameter of tree two ways

--- BT/treediameter.cpp
+++ BT/treediameter.cpp
@@ -107,6 +107,36 @@ int diameter(BTNode<int>* root){
     return max(ans1, max(ans2, ans3));
 }
 
+pair<int,int> height1(BTNode<int>*root){
+    if(root == NULL){
+        pair<int,int>p;
+        p.first=0;
+        p.second=0;
+        return p;
+    }
+    pair<int,int>leftans = height1(root->left);
+    pair<int,int>rightans = height1(root->right);
+
+    int lh = leftans.first;
+    int ld = leftans.second;
+
+    int rh=rightans.first;
+    int rd=rightans.second;
+
+    int height = 1+max(lh,rh);
+    int diameter = max(lh+rh,max(ld,rd));
+    pair<int,int>p;
+    p.first = height;
+    p.second = diameter;
+
+    return p;
+}
+
+int diameterother(BTNode<int>*root){
+    pair<int,int>p = height1(root);
+    return p.second;
+}
+
 int main(){
     BTNode<int>*root = inputlevel();
 
@@ -119,6 +149,7 @@ int main(){
     }
 
     cout<<"Diameter of the tree is: "<<diameter(root)<<endl;
+    cout<<"Other way diameter: "<<diameterother(root)<<endl;
  }
 
 
