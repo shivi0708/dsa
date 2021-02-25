c20d417bec084cc42d9c283415133e9219d5fdc9
shivi0708 <shivi.sv8@gmail.com>
2021-02-25 22:51:46 +0530

    DFS

--- graph/adjmatrix.cpp
+++ graph/adjmatrix.cpp
@@ -1,6 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void print(vector<vector<int>>v, int sv,vector<bool>&visited){
+cout<<sv<<endl;
+visited[sv]=true;
+int n = v.size();
+for(int i=0;i<n;i++){
+    if(v[sv][i]==1 && visited[i]==false){
+        print(v,i,visited);
+    }
+}
+}
+
 int main(){
     int vertices,edges;
     cin>>vertices>>edges;
@@ -11,4 +22,7 @@ int main(){
         matrix[fv][sv]=1;
         matrix[sv][fv]=1;
     }
+    vector<bool>visited(vertices,false);
+
+    print(matrix, 0,visited);
 }
